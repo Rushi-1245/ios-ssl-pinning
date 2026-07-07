21342ebe8d831a0935e536597159769dbfee0ff1
Rushi-1245 <rushikeshgaikwad4899@gmail.com>
2026-07-07 17:14:20 +0530

    Initial Commit

---
+++ NetworkSecurity/NetworkSecurity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkSecurity
+//
+//  Created by Rushi on 07/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkSecurity/NetworkSecurity/NetworkSecurityApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSecurityApp.swift
+//  NetworkSecurity
+//
+//  Created by Rushi on 07/07/26.
+//
+
+import SwiftUI
+
+@main
+struct NetworkSecurityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
